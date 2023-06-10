3f78afcbb47ee25285f08b34bd664e1a231bc992
dwenegar <simone.livieri@gmail.com>
2023-06-11 07:27:36 +0900

    simplify version check

--- src/input-helper.ts
+++ src/input-helper.ts
@@ -1,44 +1,25 @@
 import * as core from '@actions/core';
+import * as semver from 'semver';
 import {getInput} from '@actions/core';
 import {InputNames} from './constants';
 import {Inputs} from './inputs';
-
-const VALID_DOKU_VERSIONS: string[] = ['1.1.0'];
-const DOKU_VERSION_ALIASES: {[index: string]: string} = {
-  latest: '1.1.0'
-};
-
-const VALID_DOCFX_VERSIONS: string[] = ['2.67.2'];
-const DOCFX_VERSION_ALIASES: {[index: string]: string} = {
-  latest: '2.67.2'
-};
 
 export function getInputs(): Inputs {
   const dokuVersion = getInput(InputNames.DokuVersion, {required: true});
   const docFxVersion = getInput(InputNames.DocFxVersion, {required: true});
   const repoToken = getInput(InputNames.RepoToken, {required: false});
 
-  let resolvedDokuVersion = dokuVersion;
-  if (DOKU_VERSION_ALIASES[dokuVersion]) {
-    resolvedDokuVersion = DOKU_VERSION_ALIASES[dokuVersion];
+  if (!semver.valid(dokuVersion)) {
+    core.setFailed(`Invalid doku version format: ${dokuVersion}`);
   }
 
-  if (!VALID_DOKU_VERSIONS.includes(resolvedDokuVersion)) {
-    core.setFailed(`Invalid doku version: ${dokuVersion}`);
-  }
-
-  let resolvedDocFxVersion = docFxVersion;
-  if (DOCFX_VERSION_ALIASES[docFxVersion]) {
-    resolvedDocFxVersion = DOCFX_VERSION_ALIASES[docFxVersion];
-  }
-
-  if (!VALID_DOCFX_VERSIONS.includes(resolvedDocFxVersion)) {
-    core.setFailed(`Invalid DocFx version: ${docFxVersion}`);
+  if (!semver.valid(docFxVersion)) {
+    core.setFailed(`Invalid DocFx version format: ${docFxVersion}`);
   }
 
   return {
-    docFxVersion: resolvedDocFxVersion,
-    dokuVersion: resolvedDokuVersion,
+    docFxVersion: dokuVersion,
+    dokuVersion: docFxVersion,
     repoToken: repoToken
   };
 }
